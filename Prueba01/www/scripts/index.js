32a2aeae9a29e81c3b076d6b988cb4af44f59504
Damián <sch.damian@hotmail.com>
2018-01-29 23:07:39 -0300

    Cambio Estilo!!

--- Prueba01/www/scripts/index.js
+++ Prueba01/www/scripts/index.js
@@ -5,27 +5,15 @@
 (function () {
     "use strict";
 
-    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-
-    //desarrollo
-    // document.getElementById("btnBuscar").addEventListener('click', BuscarUsuario, false);
-    // document.getElementById("btnCargar").addEventListener('click', CargarLista, false);
+    //document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
 
     function onDeviceReady() {
         // Controlar la pausa de Cordova y reanudar eventos
-        document.addEventListener('pause', onPause.bind(this), false);
-        document.addEventListener('resume', onResume.bind(this), false);
-
+        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener( 'resume', onResume.bind( this ), false );
+        
         // TODO: Cordova se ha cargado. Haga aquí las inicializaciones que necesiten Cordova.
-        //Produccion
-        document.getElementById("btnBuscar").addEventListener('click', BuscarUsuario, false);
-        document.getElementById("btnCargar").addEventListener('click', CargarLista, false);
-
-        //var parentElement = document.getElementById('deviceready');
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
+        
     };
 
     function onPause() {
@@ -35,50 +23,4 @@
     function onResume() {
         // TODO: esta aplicación se ha reactivado. Restaure el estado de la aplicación aquí.
     };
-
-    function CargarLista() {
-        var cadena = "<table border=0 cellpadding=2 cellspacing=0><tr><th>ID</th><th>Mensaje</th>";
-
-        $.ajax({
-            type: "GET",
-            url: "http://Gestion1Prueba.somee.com/api/MessagesApi/",
-            crossDomain: true,
-            cache: false,
-            contentType: "application/json; charset=utf-8",
-            async: false,
-            dataType: "json",
-            success: function (data) {
-                $.each(data, function (i, field) {
-                    cadena = cadena + "<tr>" + "<td>" + field.MessageID + "</td><td>" + field.Descripcion + "</td>";
-                });
-                cadena = cadena + "</table>";
-                $("#divLista").append(cadena);
-            },
-            error: function (data) {
-                alert("Ocurrio un problema");
-            }
-        })
-    };
-
-    function BuscarUsuario() {
-        var id = document.getElementById("txtNombre").value;
-        if (id == "") {
-            alert("Ingrese un id");
-        }
-        else {
-            $.ajax({
-                type: "GET",
-                url: "http://Gestion1Prueba.somee.com/api/MessagesApi/" + id,
-                crossDomain: true,
-                cache: false,
-                success: function (data) {
-                    document.getElementById("divResultado").innerHTML = "Mensaje : " + data.Descripcion;
-                },
-                error: function (data) {
-                    alert("Ocurrio un problema");
-                }
-            })
-        }
-    }
-
-})();
+} )();
